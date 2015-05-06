1c9340675532ea43bb4a712bcb4158ca01255ee3
Peter LaValle <peter.lavalle@gmail.com>
2015-05-06 22:56:38 +0100

    started a .jimp writer
    spits out identical outputs to the input
    
    (self verification FTW!)

---
+++ cook/src/test/scala/peterlavalle/jimpifier/cook/TCookTest.scala
@@ -0,0 +1,36 @@
+package peterlavalle.jimpifier.cook
+
+import java.io.{Writer, StringWriter}
+
+import jimp.TJimpTests
+import junit.framework.TestCase
+import junit.framework.Assert._
+import org.easymock.EasyMock
+import peterlavalle.jimpifier.ast.Module
+import peterlavalle.jimpifier.compile.{Syntax, TAntlr4Parser}
+
+import scala.io.Source
+
+trait TCookTest extends TestCase with TJimpTests with TAntlr4Parser {
+
+	val cook: TCook
+
+	override def apply(sourceName: String): Unit = {
+
+		val stringWriter = new StringWriter()
+
+		val mockOutput = EasyMock.createMock(classOf[String => Writer])
+
+		EasyMock.expect(mockOutput(sourceName)).andReturn(stringWriter).once()
+
+		EasyMock.replay(mockOutput)
+		cook.cook(mockOutput)(Syntax(parser(sourceName).module()))
+
+		EasyMock.verify(mockOutput)
+
+		assertEquals(
+			Source.fromInputStream(classOf[TJimpTests].getResourceAsStream(sourceName)).mkString.replaceAll("\r?\n", "\n"),
+			stringWriter.toString.replaceAll("\r?\n", "\n")
+		)
+	}
+}
